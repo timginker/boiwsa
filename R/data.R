aa56bd02f5b2a6154bc78abcf2820843ccfc9077
Tim Ginker <timginker@gmail.com>
2023-02-26 11:46:13 +0200

    added data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' US finished motor gasoline product supplied
+#'
+#' Weekly data beginning 2 February 1991, ending 20 January 2017. Units are "million barrels per day".
+#' Report ...
+#'
+#' @format ## Data.Frame
+#' A data frame with 1355 rows and 2 columns:
+#' \describe{
+#'   \item{date}{date in a date format}
+#'   \item{y}{gasoline consumption}
+#' }
+#' @source fpp2 package
+"gasoline.data"
