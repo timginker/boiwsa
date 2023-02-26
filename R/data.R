c2dbfab84a285bdaa5d39db374446a34aa1c1e4b
Tim Ginker <timginker@gmail.com>
2023-02-26 12:40:10 +0200

    added data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,27 @@
 #' }
 #' @source fpp2 package
 "gasoline.data"
+
+#' @title Israeli working dates
+#' @description Israeli working dates
+#' @format A data frame with 21550 rows and 4 variables:
+#' \describe{
+#'   \item{\code{DATE_VALUE}}{Date}
+#'   \item{\code{ISR_WORKING_DAY_PART}}{1: full working day, 0.5: half working day, 0: holiday}
+#'   \item{\code{JEWISH_FULL_DATE}}{Jewish date}
+#'   \item{\code{DATE_WEEK_NUMBER}}{Weekday}
+#'}
+#' @source Personal
+"dates_il"
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 51 rows and 3 variables:
+#' \describe{
+#'   \item{\code{year}}{Year}
+#'   \item{\code{rosh}}{Rosh Hashanah date}
+#'   \item{\code{pesah}}{Pesach date}
+#'}
+#' @source Personal
+"holiday_dates_il"
+
