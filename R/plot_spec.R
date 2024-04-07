ff01a50eda72c4630269d1049fd3adc568e81d75
Tim Ginker <timginker@gmail.com>
2024-04-07 15:59:39 +0300

    fixed a typo in description

--- R/plot_spec.R
+++ R/plot_spec.R
@@ -28,9 +28,9 @@ plot_spec=function(x){
     ggplot2::geom_line(aes(x=spec0$freq,y=spec0$spec,color="orig"))+
     ggplot2::geom_line(aes(x=spec0$freq,y=spec1$spec,color="sa"))+
     ggplot2::geom_vline(xintercept = 1:2/4.34,linetype="dashed")+
-    ggplot2::geom_text(aes(x=1:2/4.34, label="\n Intra-monthly cycle peacks", y=0.5*max(spec0$spec)), colour="black", angle=90)+
+    ggplot2::geom_text(aes(x=1:2/4.34, label="\n Intra-monthly cycle peaks", y=0.5*max(spec0$spec)), colour="black", angle=90)+
     ggplot2::geom_vline(xintercept = (1:3)/52.1775,linetype="dashed")+
-    ggplot2::geom_text(aes(x=3/52.1775, label="\n First three intra-yearly cycle peacks", y=0.5*max(spec0$spec)), colour="black", angle=90)+
+    ggplot2::geom_text(aes(x=3/52.1775, label="\n First three intra-yearly cycle peaks", y=0.5*max(spec0$spec)), colour="black", angle=90)+
     ggplot2::scale_color_manual(name = "",
                                 values = c( "orig"='#31a354', "sa"="#3182bd"),
                                 labels = c("Original", "Seasonally adjusted"))+
